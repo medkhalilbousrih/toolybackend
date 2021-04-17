206f7c0597e58d15304978ec077ebcc81eba1033
Khalil <medkhalilbousrih@gmail.com>
2021-04-17 03:13:59 +0100

    cors

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const loginRouter = require("./controllers/login");
 const middleware = require("./utils/middleware");
 const toolRouter = require("./controllers/tools");
 const adminRouter = require("./controllers/admins");
-
+const cors = require("cors");
 //app.use(express.static(path.join("build")));
-
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
