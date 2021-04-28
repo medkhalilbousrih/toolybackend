49dda2b992608021e953684f989f469dda65991d
Khalil <medkhalilbousrih@gmail.com>
2021-04-28 08:22:16 +0100

    fix

--- utils/middleware.js
+++ utils/middleware.js
@@ -6,7 +6,7 @@ const errorHandler = (err, req, res, next) => {
   res.status(500).end(err.message);
 };
 const unknownEndpoint = (req, res) => {
-  res.status(404).end();
+  res.redirect("/");
 };
 
 const userExtractor = async (req, res, next) => {
