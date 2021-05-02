39cbf082f018f004cc358155f72a0cd59f657624
Khalil <medkhalilbousrih@gmail.com>
2021-05-02 04:20:27 +0100

    env

--- utils/image-classification.js
+++ utils/image-classification.js
@@ -2,9 +2,6 @@ const got = require("got");
 const fs = require("fs");
 const FormData = require("form-data");
 
-const apiKey = "acc_d5b63a8f1b439d0";
-const apiSecret = "95319d16977e9304c0d891b83616fdbe";
-
 const classify = async (url) => {
   try {
     const filePath = `./build${url}`;
@@ -12,8 +9,8 @@ const classify = async (url) => {
     formData.append("image", fs.createReadStream(filePath));
     const response = await got.post("https://api.imagga.com/v2/tags", {
       body: formData,
-      username: apiKey,
-      password: apiSecret,
+      username: process.env.APIKEYIMAGGA,
+      password: process.env.APISECRETIMAGGA,
     });
     return response.body;
   } catch (error) {
