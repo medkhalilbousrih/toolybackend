fee1a7d6f37286a6b8629772b4b4a7d45cc08444
Khalil <medkhalilbousrih@gmail.com>
2021-05-04 01:34:02 +0100

    fix

--- utils/email-verification.js
+++ utils/email-verification.js
@@ -25,7 +25,7 @@ const sendVerif = async (id, email) => {
     
     Click below to confirm your email address:
     
-    <a href="https://tooly-tn.herokuapp.com/api/users/confirmation/${token}">https://tooly-tn.herokuapp.com/api/users/confirmation</a>
+    <a href="http://localhost:3001/api/users/confirmation/${token}">https://tooly-tn.herokuapp.com/api/users/confirmation</a>
     
     If you have problems, please paste the above URL into your web browser.`, // html body
   });
